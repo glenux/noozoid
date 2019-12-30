76a9316e5c483b90add11f02192efdf8d8a71b2a
Glenn Y. Rolland <glenux@glenux.net>
2019-12-30 01:55:17 +0100

    Remove whitespaces

--- lib/noozoid/config.rb
+++ lib/noozoid/config.rb
@@ -1,34 +1,35 @@
+# frozen_string_literal: true
 
 module Noozoid
   module Config
     HELP_KEYS = [
-      [:nav_parent,  'Navigate to parent'],
-      [:nav_child,   'Navigate to child' ],
-      [:nav_next,    'Navigate to next sibling'],
-      [:nav_previous,    'Navigate to previous sibling'],
-      [:nav_root,    'Navigate to tree root'],
-      [:nav_none,    ''],
-      [:node_create, 'Create node'],
-      [:node_delete, 'Delete selected node'],
-      [:node_toggle, 'Toggle node'],
-      [:node_none,   ''],
-      [:main_help,   'Show this help'],
-      [:main_quit,   'Exit program']
-    ]
+      [:nav_parent,   'Navigate to parent'],
+      [:nav_child,    'Navigate to child'],
+      [:nav_next,     'Navigate to next sibling'],
+      [:nav_previous, 'Navigate to previous sibling'],
+      [:nav_root,     'Navigate to tree root'],
+      [:nav_none,     ''],
+      [:node_create,  'Create node'],
+      [:node_delete,  'Delete selected node'],
+      [:node_toggle,  'Toggle node'],
+      [:node_none,    ''],
+      [:main_help,    'Show this help'],
+      [:main_quit,    'Exit program']
+    ].freeze
 
     DEFAULT_KEYS = {
-      nav_parent:   'LEFT',
-      nav_child:    'RIGHT',
-      nav_next:     'DOWN',
+      nav_parent: 'LEFT',
+      nav_child: 'RIGHT',
+      nav_next: 'DOWN',
       nav_previous: 'UP',
-      nav_root:     'r',
-      nav_none:     '',
-      node_create:  'a',
-      node_delete:  'd',
-      node_toggle:  'v',
-      node_none:    '',
-      main_quit:    'q',
-      main_help:    '?'
-    }
+      nav_root: 'r',
+      nav_none: '',
+      node_create: 'a',
+      node_delete: 'd',
+      node_toggle: 'v',
+      node_none: '',
+      main_quit: 'q',
+      main_help: '?'
+    }.freeze
   end
 end
